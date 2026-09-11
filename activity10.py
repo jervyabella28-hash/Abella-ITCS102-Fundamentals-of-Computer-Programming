447c339a11888e25b200533959c709a9f98fd835
jervyabella28-hash <jervyabella28@gmail.com>
2026-09-11 11:09:49 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,12 @@
+# basic if else program
+
+username = "jervy"
+password = "123"
+
+u = input("input USERNAME :")
+p = input("input PASSWORD :")
+
+if u == username and p == password :
+	print("username and password correct")
+else:
+	print("ACCESS DENIED")
