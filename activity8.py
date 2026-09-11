d82b7c7041d17e49e724f8c7aa6b582b2b00077a
jervyabella28-hash <jervyabella28@gmail.com>
2026-09-11 11:01:45 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,30 @@
+hobbies = ""
+
+a = input("What are your hobbies?")
+hobbies += a + ","
+
+b = input("What are your hobbies?")
+hobbies += b + ","
+
+c = input("What are your hobbies?")
+hobbies += c + ","
+
+d = input("What are your hobbies?")
+hobbies += d + ","
+
+e = input("What are your hobbies?")
+hobbies += e + ","
+
+f = input("What are your hobbies?")
+hobbies += f + ","
+
+g = input("What are your hobbies?")
+hobbies += g + ","
+
+h = input("What are your hobbies?")
+hobbies += h + ","
+
+i = input("What are your hobbies?")
+hobbies += i + ","
+
+print("Your hobbies are (", hobbies ,")")
