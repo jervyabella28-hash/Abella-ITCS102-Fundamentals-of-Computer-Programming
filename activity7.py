56c5f33c53025d33cb4f85336382a4deab63aae5
jervyabella28-hash <jervyabella28@gmail.com>
2026-09-11 10:56:52 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,28 @@
+total = 28
+
+Z = eval(input("\nEnter any number:"))
+total = total + Z
+
+A = eval(input("\nanother number:"))
+total += A
+
+B = eval(input("\nanother number:"))
+total -= B
+
+C = eval(input("\nanother number:"))
+total *= C
+
+D = eval(input("\nanother number:"))
+total -= D
+
+E = eval(input("\nanother number:"))
+total *= E
+
+F = eval(input("\nanother number:"))
+total += F
+
+G = eval(input("\nanother number:"))
+total *= G
+
+print(type(total))
+print("The total is", total)
