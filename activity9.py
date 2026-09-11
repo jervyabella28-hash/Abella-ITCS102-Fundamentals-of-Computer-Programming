af61f364f2f75d1ebb32c1aa2a5b4557fb11c289
jervyabella28-hash <jervyabella28@gmail.com>
2026-09-11 11:05:54 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,24 @@
+# relational operator activity
+
+x = 6
+y = 7
+
+password1 = "1"
+password2 = "2"
+
+#1
+print(x > y)
+
+#2
+print(x < y)
+
+#3
+print(x >= y)
+
+#4
+print(x <= y)
+
+#5
+print( x== y)
+
+print(password1 != password2)
